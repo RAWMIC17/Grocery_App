83e34f5587785cfd6c70b1310c0e0a898621278b
RAWMIC17 <hks1710harsh@gmail.com>
2023-12-17 19:32:31 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app_demo/Pages/splash_screen.dart';
+import 'package:grocery_app_demo/utils/routes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(titleTextStyle: TextStyle(fontWeight: FontWeight.w100),
-        ),
-      body: Center(
-        child: Text('Grocery App!'),
-      ),
-      )
-    );
+      initialRoute: MyRoutes.splashRoute,
+      routes: {"/":(context) =>  SplashPage(),
+      MyRoutes.splashRoute: (context) => SplashPage(),
+      }
+      );
   }
 }
